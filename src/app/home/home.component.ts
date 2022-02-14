c5098429a3a76bc0418f7dab27bb278c4cbe0d09
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-14 14:23:19 +0300

    add user & repo constructors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Repository } from '../repository';
+import { User } from '../user';
+import { UserRequestService } from '../user-http/user-request.service';
+import { UserService } from '../user-service/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [UserService]
 })
 export class HomeComponent implements OnInit {
+  // users: User[];
+  user!: User;
+  username!: string;
+  userService!: UserRequestService;
+  repos!: Repository;
 
-  constructor() { }
+  searchNewUser(user: User){
+    this.userService.userRequest(this.user.username)
+  }
+
+  constructor(private router: Router, gitUserService: UserRequestService) {
+    this.userService = gitUserService;
+  }
 
   ngOnInit(): void {
+    this.userService.userRequest(this.username);
+    this.user = this.userService.user;
+    console.log(this.user);
+
+    this.userService.repoRequest(this.username);
+    this.repos = this.userService.repos;
+    console.log(this.repos)
+    
   }
 
 }
--- src/app/repo-search/repo-search.component.ts
+++ src/app/repo-search/repo-search.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Repository } from '../repository';
+import { User } from '../user';
+import { UserRequestService } from '../user-http/user-request.service';
 
 @Component({
   selector: 'app-repo-search',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
+  user!: User
+  repos!: Repository
 
-  constructor() { }
+  constructor(private http: HttpClient, private userService: UserRequestService) {
+    
+  }
 
   ngOnInit(): void {
+    this.userService.userRequest(this.user.username)
+    this.user = this.userService.user
+    
   }
 
 }
