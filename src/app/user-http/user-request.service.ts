77403277ca5963ab65e1bf85c87d35ccaf4f42da
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-14 22:25:16 +0300

    func to the user service

--- src/app/user-http/user-request.service.ts
+++ src/app/user-http/user-request.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { User } from '../user';
+import { Repository } from '../repository';
+// import { Users } from '../userList';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRequestService {
 
-  constructor() { }
+  user!: User
+  repos!: Repository
+
+  constructor(private http: HttpClient) {
+    this.user = new User("", "", "", "")
+    this.repos = new Repository("", "", "", "", "")
+  }
+
+  userRequest(username: string){
+    interface ApiResponse{
+      bio: string;
+      login: string;
+      avatar_url: string;
+    }
+
+    let promise = new Promise((resolve, reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl + `${username}?` + environment.access_token).toPromise().then(response=>{
+        
+        if(response){
+          console.log(response)
+          this.user.username = response.login
+          this.user.avatar_url = response.avatar_url
+          this.user.bio = response.bio
+          resolve(this.userRequest)
+        }
+      },
+      error=>{
+        this.user.username = "User not found"
+
+        reject(error)
+      })
+    })
+    return promise
+  }
+
+  repoRequest(username: string){
+    let promise = new Promise((resolve, reject)=>{
+      let repoUrl = environment.apiUrl + `${username}` + '/repos';
+      this.http.get<any>(repoUrl).toPromise().then(res=>{
+        if(res){
+          console.log(res)
+          resolve(this.repoRequest)
+        }
+      },
+      error=>{
+        reject(error)
+      })
+    })
+    return promise
+  }
 }
