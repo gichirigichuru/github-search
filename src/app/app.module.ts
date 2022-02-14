90acc2420f6c5c31f096fb6a10f5e0f0c369f4a4
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-14 13:55:52 +0300

    added repo-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RepoSearchComponent
   ],
   imports: [
     BrowserModule,
